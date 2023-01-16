468aecb359be584986c50ad44aeff8413419fd9a
Thomas Kuschan <mail@thomaskuschan.de>
2023-01-16 20:36:09 +0100

    Implemented interactive mode for user creation

--- src/Commands/CreateUser.php
+++ src/Commands/CreateUser.php
@@ -15,8 +15,8 @@ class CreateUser extends Command
      * @var string
      */
     protected $signature = 'user:create 
-        {name : The name of the new user}
-        {email : The email for the new user}
+        {name? : The name of the new user}
+        {email? : The email for the new user}
         {password? : The password to be set for the new user}
         {--hide-password : Always hide the password in output}
         {--show-password : Always show the password in output}
@@ -27,7 +27,7 @@ class CreateUser extends Command
      *
      * @var string
      */
-    protected $description = 'Create a new user in the database';
+    protected $description = 'Create a new user in the database. Call without parameters for interactive user creation.';
 
     /**
      * Execute the console command.
@@ -42,25 +42,33 @@ class CreateUser extends Command
             return Command::FAILURE;
         }
 
+        // see if we should do this interactively
+        $interactive = $this->argument('user') == null;
+        $user = new User();
+
+        // insert data from input
+        $user->name = $this->argument('name') ?? $this->anticipate('User Name?', ['John Doe'], 'John Doe');
+        $user->email = $this->argument('email') ?? $this->anticipate('User Email?', ['mail@example.com'], 'mail@example.com');
+
         // check for the email to be unique
-        if(User::where('email', $this->argument('email'))->first()) {
+        if(User::where('email', $user->email)->first()) {
             $this->error('Cannot create user with this email!');
             return Command::FAILURE;
         }
-
-        // insert data from input
-        $user = new User();
-        $user->name = $this->argument('name');
-        $user->email = $this->argument('email');
         
         // check for password
         $hasPasswordInput = $this->argument('password') != null;
+        if($interactive) {
+            // ask whether it should be random or not
+            $hasPasswordInput = $this->confirm('Generate random password?', true) == false;
+        }
         $passwordClearText = '';
         if($hasPasswordInput) {
-            $passwordClearText = $this->argument('password');
+            $passwordClearText = $this->argument('password') ?? $this->secret('User Password?');
         }
         else {
             // create a random password
+            $this->line('Generating random password');
             $passwordClearText = substr(base64_encode(md5(rand())), 0, 16);
         }
 
